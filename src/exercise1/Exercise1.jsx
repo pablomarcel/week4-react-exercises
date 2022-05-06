6a841ec202190cd1ed5939cdb62fe567350bd538
pablomarcel <pablomarcel@outlook.com>
2022-05-05 21:30:39 -0700

    Exercise1.jsx and Exercise2.jsx completed, tests passed

--- src/exercise1/Exercise1.jsx
+++ src/exercise1/Exercise1.jsx
@@ -1,16 +1,50 @@
 import React from 'react';
+import CoffeeCard from "./CoffeeCard";
 
 export default function Exercise1() {
+    const drinks =[
+        {
+            drinkName: 'espresso',
+            price:1
+        },
+        {
+            drinkName: 'americano',
+            price:2
+        },
+        {
+            drinkName: 'latte',
+            price:3
+        },
+        {
+            drinkName: 'mocha',
+            price:4
+        }
+    ]
+
+
   return (
     <div>
-      <h2>Exercise 1</h2>
-      <p>
-        In <code>Exercise1.jsx</code>, there is a component called <code>CoffeeCard.jsx</code> that is imported, but not used.
-      </p>
-      <ol>
-        <li>Render this component four times in <code>Exercise1.jsx</code> (one for each coffee drink).</li>
-        <li>Pass in the correct props.</li>
-      </ol>
+        {/*add a loop*/}
+
+        {/* eslint-disable-next-line array-callback-return */}
+        {drinks.map((item, index)=>{
+            return <CoffeeCard key={index} price={item.price} drinkName={item.drinkName}/>
+    })}
+
+        {/*<CoffeeCard price={'1'} drinkName={'espresso'}/>*/}
+        {/*<CoffeeCard price={'2'} drinkName={'mocha'}/>*/}
+        {/*<CoffeeCard price={'3'} drinkName={'latte'}/>*/}
+        {/*<CoffeeCard price={'4'} drinkName={'americano'}/>*/}
+      {/*<h2>Exercise 1</h2>*/}
+      {/*<p>*/}
+      {/*  In <code>Exercise1.jsx</code>, there is a component called <code>CoffeeCard.jsx</code> that is imported, but not used.*/}
+      {/*</p>*/}
+      {/*<ol>*/}
+
+      {/*  /!*<li>Render this component four times in <code>Exercise1.jsx</code> (one for each coffee drink).</li>*!/*/}
+      {/*  /!*<li>Pass in the correct props.</li>*!/*/}
+      {/*    */}
+      {/*</ol>*/}
     </div>
   );
 }
--- src/exercise2/Orchid.jsx
+++ src/exercise2/Orchid.jsx
@@ -1,2 +1,23 @@
 // Here's a URL to get you started
+import React from "react";
+import PropTypes from "prop-types";
 const orchidImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/d/df/Orchid_high_resolution.jpg';
+
+export default function Orchid( {borderColor} ){
+    console.log(borderColor)
+    return (
+    <div>
+        <img style={{
+            borderColor: borderColor,
+            // borderColor: '#ff0000',
+            borderStyle: "solid",
+            // color: '#ff0000',
+            width: '400px',
+        }}  src = {orchidImageUrl} alt="orchid" />
+    </div>
+)
+}
+
+Orchid.propTypes ={
+    borderColor: PropTypes.string.isRequired
+}
